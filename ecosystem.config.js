890d64e06b625a9b67171e896de716cce2a1f217
MattEagle95 <55457634+MattEagle95@users.noreply.github.com>
2021-05-01 00:53:46 +0200

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,33 +1,32 @@
 module.exports = {
     apps: [
-      {
-        name: 'watchyolog',
-        script: 'npm start',
-        time: true,
-        instances: 1,
-        autorestart: true,
-        max_restarts: 50,
-        watch: false,
-        max_memory_restart: '1G',
-        env: {
-          PORT: 3000,
-          DATABASE_ADDRESS: process.env.DATABASE_ADDRESS
+        {
+            name: 'watchyolog',
+            script: 'npm start',
+            time: true,
+            instances: 1,
+            autorestart: true,
+            max_restarts: 50,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                PORT: 3000
+            },
         },
-      },
     ],
     deploy: {
-      production: {
-        user: 'nodejs',
-        host: '139.59.211.241',
-        key: 'deploy.key',
-        ref: 'origin/main',
-        repo: 'https://github.com/MattEagle95/watchyolog',
-        path: '/var/www/nodejs/watchyolog',
-        'post-deploy':
-          'npm install && pm2 reload ecosystem.config.js --env production && pm2 save',
-        env: {
-          NODE_ENV: 'production'
+        production: {
+            user: 'nodejs',
+            host: '139.59.211.241',
+            key: 'deploy.key',
+            ref: 'origin/main',
+            repo: 'https://github.com/MattEagle95/watchyolog',
+            path: '/var/www/nodejs/watchyolog',
+            'post-deploy':
+                'npm install; pm2 reload ecosystem.config.js --env production; pm2 save',
+            env: {
+                NODE_ENV: 'production'
+            },
         },
-      },
     },
-  }
+}
