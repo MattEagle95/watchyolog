0aebdad54cbaae6eecb4e1d8f84e96eb52de9e5a
MattEagle95 <55457634+MattEagle95@users.noreply.github.com>
2021-05-01 01:36:44 +0200

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,8 +22,9 @@ module.exports = {
             ref: 'origin/main',
             repo: 'https://github.com/MattEagle95/watchyolog',
             path: '/var/www/nodejs/watchyolog',
-            'pre-deploy': "git fetch && git reset --hard origin/dev",
-            'post-deploy':
+            "pre-setup" : "git fetch && git reset --hard origin/dev",
+            "pre-deploy": "git fetch && git reset --hard origin/dev",
+            "post-deploy":
                 'echo "test" > test.key; pm2 startOrRestart source/ecosystem.config.js --env production; pm2 save',
             env: {
                 NODE_ENV: 'production'
