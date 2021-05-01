040863bd30926e22de65f692c3ad70a734eb57df
MattEagle95 <55457634+MattEagle95@users.noreply.github.com>
2021-05-01 15:15:31 +0200

    1

--- index.js
+++ index.js
@@ -27,9 +27,11 @@ client.on("message", function (message) {
         
             pm2.list((err, list) => {
                 let output = "";
+
+                output += `${list.filter(x => x.pm2_env.status === 'online').length}/${list.length} online`;
         
                 list.forEach(process => {
-                    output += `${process.name} ${process.pm2_env.pm_uptime} ${process.pm2_env.status} \n`;
+                    output += `${process.name} ${timeDifference(process.pm2_env.pm_uptime)} ${process.pm2_env.unstable_restarts} ${process.pm2_env.status} \n`;
                 })
         
                 message.reply(output);
@@ -38,3 +40,38 @@ client.on("message", function (message) {
         });
     }
 });
+
+function timeDifference(current, previous) {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+         return Math.round(elapsed/1000) + ' seconds ago';   
+    }
+
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+        return 'approximately ' + Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+
+    else if (elapsed < msPerYear) {
+        return 'approximately ' + Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+
+    else {
+        return 'approximately ' + Math.round(elapsed/msPerYear ) + ' years ago';   
+    }
+}
