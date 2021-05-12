2bcbf919c45f5ad598a56a369d59f695f1b6f9dc
MattEagle95 <55457634+MattEagle95@users.noreply.github.com>
2021-05-12 22:17:15 +0200

    1

--- index.js
+++ index.js
@@ -18,6 +18,46 @@ client.on('ready', async () => {
     sendEventMsg(`I'm online`);
 });
 
+let logProcesses = [];
+
+pm2.launchBus(function (err, bus) {
+    if (err) {
+        console.error(err)
+    }
+
+    bus.on('process:event', function (packet) {
+        const channel = client.channels.cache.find(channel => channel.name === 'general');
+        if (packet.event === 'restart' || packet.event === 'reload' || packet.event === 'exit') {
+            channel.send(`:tools: ${packet.event} - ${packet.process.name}`);
+            return;
+        }
+
+        if (packet.event === 'stop') {
+            channel.send(`:stop_sign: ${packet.event} - ${packet.process.name}`);
+            return;
+        }
+
+        if (packet.event === 'online') {
+            channel.send(`:green_circle: ${packet.event} - ${packet.process.name}`);
+            return;
+        }
+
+        channel.send(`EVENT: ${packet.event} - ${packet.process.name}`);
+    })
+
+    bus.on('log:out', function (packet) {
+        if (logProcesses.indexOf(packet.process.name.trim()) !== -1) {
+            const channel = client.channels.cache.find(channel => channel.name === `log-${packet.process.name.trim()}`);
+            channel.send(`${packet.data}`);
+        }
+    })
+
+    bus.on('log:err', function (packet) {
+        const channel = client.channels.cache.find(channel => channel.name === 'error-log');
+        channel.send(`${packet.process.name}: ${packet.data}`);
+    })
+})
+
 
 const sendEventMsg = async (msg) => {
     const guilds = await configService.get();
